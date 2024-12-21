6422e9961bfc5a280695fd8f29206e2f5c4a47ab
HAJITH-M <hajithm2003@gmail.com>
2024-12-21 20:37:04 +0530

    Capacitor added

---
+++ helpBot/android/app/src/main/java/com/zaraxai/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zaraxai.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- helpBot/src/App.jsx
+++ helpBot/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import WebScrapper from "./Component/WebScrapper"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import WebScrapper from "./Component/WebScrapper";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import FileExtractor from './Component/FileExtractor';
 import PrivateRoute from './AuthContext/PrivateRoute';
 import Login from './AuthContext/Login';
@@ -11,29 +11,55 @@ import ChatBot from './Component/ChatBot/ChatBot';
 import HomeComponent from './Component/HomeComponent/HomeComponent';
 import CircleMenuComponent from './Component/CircleMenuComponent/CircleMenuComponent';
 import ErrorComponent from './Component/ErrorComponent/ErrorComponent';
-
+import { App as CapacitorApp } from '@capacitor/app';
 
 const App = () => {
+ 
+
+  useEffect(() => {
+    let lastTimeBackPress = 0;
+    
+    const handleBackButton = async () => {
+      if (window.location.pathname === '/') {
+        const currentTime = new Date().getTime();
+        
+        if (currentTime - lastTimeBackPress < 2000) {
+          await CapacitorApp.exitApp();
+        } else {
+          lastTimeBackPress = currentTime;
+          // You can add a toast or alert here
+          alert('Press back again to exit');
+        }
+      } else {
+        window.history.back();
+      }
+    };
+  
+    const backButtonListener = CapacitorApp.addListener('backButton', handleBackButton);
+  
+    return () => {
+      if (backButtonListener) {
+        backButtonListener.remove();
+      }
+    };
+  }, []);
+
   return (
-    <>
     <Router>
       <Routes>
-        <Route path="/" element={<><HomeComponent /> <CircleMenuComponent/> </>} />
-        <Route path="/WebScrapper" element={<PrivateRoute><><WebScrapper /> <CircleMenuComponent/> </></PrivateRoute>} />
-        <Route path="/fileupload" element={<PrivateRoute><><FileExtractor /> <CircleMenuComponent/></> </PrivateRoute>} />  
-        <Route path="/imagegeneration" element={<PrivateRoute><><ImageComponent /> <CircleMenuComponent/></> </PrivateRoute>} />  
-        <Route path="/chatbot" element={<PrivateRoute><><ChatBot /> <CircleMenuComponent/></> </PrivateRoute>} />  
+        <Route path="/" element={<><HomeComponent /> <CircleMenuComponent /> </>} />
+        <Route path="/WebScrapper" element={<PrivateRoute><><WebScrapper /> <CircleMenuComponent /> </></PrivateRoute>} />
+        <Route path="/fileupload" element={<PrivateRoute><><FileExtractor /> <CircleMenuComponent /></> </PrivateRoute>} />
+        <Route path="/imagegeneration" element={<PrivateRoute><><ImageComponent /> <CircleMenuComponent /></> </PrivateRoute>} />
+        <Route path="/chatbot" element={<PrivateRoute><><ChatBot /> <CircleMenuComponent /></> </PrivateRoute>} />
         <Route path="/logout" element={<PrivateRoute><LogOut /></PrivateRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
         {/* error page */}
-        <Route path="*" element={<ErrorComponent/>} />
+        <Route path="*" element={<ErrorComponent />} />
       </Routes>
     </Router>
-    </>
-    
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
